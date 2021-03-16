krisplusmagger/radio

commit 1187e66f23b4408ef2e1accdfb3fcb3e1bd6eea0

qtgui: Remove manual memory management
Signed-off-by: Thomas Habets <thomas@habets.se>

gr-qtgui/lib/time_sink_c_impl.cc
@@ -56,32 +56,18 @@ time_sink_c_impl::time_sink_c_impl(int size,
     if (nconnections > 12)
         throw std::runtime_error("time_sink_c only supports up to 12 inputs");
 
-    // Required now for Qt; argc must be greater than 0 and argv
-    // must have at least one valid character. Must be valid through
-    // life of the qApplication:
-    // http://harmattan-dev.nokia.com/docs/library/html/qt4/qapplication.html
-    d_argc = 1;
-    d_argv = new char;
-    d_argv[0] = '\0';
-
-    d_main_gui = NULL;
-
     // setup PDU handling input port
     message_port_register_in(pmt::mp("in"));
     set_msg_handler(pmt::mp("in"), [this](pmt::pmt_t msg) { this->handle_pdus(msg); });
 
     // +2 for the PDU message buffers
     for (unsigned int n = 0; n < d_nconnections + 2; n++) {
-        d_buffers.push_back(
-            (double*)volk_malloc(d_buffer_size * sizeof(double), volk_get_alignment()));
-        memset(d_buffers[n], 0, d_buffer_size * sizeof(double));
+        d_buffers.emplace_back(d_buffer_size);
     }
 
     // We don't use cbuffers with the PDU message handling capabilities.
     for (unsigned int n = 0; n < d_nconnections / 2; n++) {
-        d_cbuffers.push_back((gr_complex*)volk_malloc(d_buffer_size * sizeof(gr_complex),
-                                                      volk_get_alignment()));
-        std::fill_n(d_cbuffers[n], d_buffer_size, 0);
+        d_cbuffers.emplace_back(d_buffer_size);
     }
 
     // Set alignment properties for VOLK
@@ -103,16 +89,6 @@ time_sink_c_impl::~time_sink_c_impl()
 {
     if (!d_main_gui->isClosed())
         d_main_gui->close();
-
-    // d_main_gui is a qwidget destroyed with its parent
-    for (unsigned int n = 0; n < d_nconnections + 2; n++) {
-        volk_free(d_buffers[n]);
-    }
-    for (unsigned int n = 0; n < d_nconnections / 2; n++) {
-        volk_free(d_cbuffers[n]);
-    }
-
-    delete d_argv;
 }
 
 bool time_sink_c_impl::check_topology(int ninputs, int noutputs)
@@ -302,17 +278,10 @@ void time_sink_c_impl::set_nsamps(const int newsize)
 
         // Resize buffers and replace data
         for (unsigned int n = 0; n < d_nconnections + 2; n++) {
-            volk_free(d_buffers[n]);
-            d_buffers[n] = (double*)volk_malloc(d_buffer_size * sizeof(double),
-                                                volk_get_alignment());
-            memset(d_buffers[n], 0, d_buffer_size * sizeof(double));
-        }
-
-        for (unsigned int n = 0; n < d_nconnections / 2; n++) {
-            volk_free(d_cbuffers[n]);
-            d_cbuffers[n] = (gr_complex*)volk_malloc(d_buffer_size * sizeof(gr_complex),
-                                                     volk_get_alignment());
-            std::fill_n(d_cbuffers[n], d_buffer_size, 0);
+            d_buffers[n].clear();
+            d_buffers[n].resize(d_buffer_size);
+            d_cbuffers[n].clear();
+            d_cbuffers[n].resize(d_buffer_size);
         }
 
         // If delay was set beyond the new boundary, pull it back.
@@ -391,7 +360,7 @@ void time_sink_c_impl::_reset()
             // represents data that might have to be plotted again if a
             // trigger occurs and we have a trigger delay set.  The tail
             // section is between (d_end-d_trigger_delay) and d_end.
-            memmove(d_cbuffers[n],
+            memmove(d_cbuffers[n].data(),
                     &d_cbuffers[n][d_end - d_trigger_delay],
                     d_trigger_delay * sizeof(gr_complex));
 
@@ -588,8 +557,8 @@ int time_sink_c_impl::work(int noutput_items,
     if ((d_triggered) && (d_index == d_end)) {
         // Copy data to be plotted to start of buffers.
         for (n = 0; n < d_nconnections / 2; n++) {
-            volk_32fc_deinterleave_64f_x2(d_buffers[2 * n + 0],
-                                          d_buffers[2 * n + 1],
+            volk_32fc_deinterleave_64f_x2(d_buffers[2 * n + 0].data(),
+                                          d_buffers[2 * n + 1].data(),
                                           &d_cbuffers[n][d_start],
                                           d_size);
         }
@@ -671,8 +640,8 @@ void time_sink_c_impl::handle_pdus(pmt::pmt_t msg)
 
         set_nsamps(len);
 
-        volk_32fc_deinterleave_64f_x2(d_buffers[2 * d_nconnections + 0],
-                                      d_buffers[2 * d_nconnections + 1],
+        volk_32fc_deinterleave_64f_x2(d_buffers[2 * d_nconnections + 0].data(),
+                                      d_buffers[2 * d_nconnections + 1].data(),
                                       in,
                                       len);
 

gr-qtgui/include/gnuradio/qtgui/const_sink_c.h
@@ -137,7 +137,16 @@ public:
     virtual int nsamps() const = 0;
     virtual void reset() = 0;
 
+    // Disallow copy/move because of the pointer.
+    const_sink_c(const const_sink_c&) = delete;
+    const_sink_c& operator=(const const_sink_c&) = delete;
+    const_sink_c(const_sink_c&&) = delete;
+    const_sink_c& operator=(const_sink_c&&) = delete;
+
     QApplication* d_qApplication;
+
+protected:
+    const_sink_c() = default;
 };
 
 } /* namespace qtgui */

gr-qtgui/include/gnuradio/qtgui/spectrumUpdateEvents.h
@@ -14,6 +14,7 @@
 #include <gnuradio/high_res_timer.h>
 #include <gnuradio/qtgui/api.h>
 #include <gnuradio/tags.h>
+#include <volk/volk_alloc.hh>
 #include <QEvent>
 #include <QString>
 #include <complex>
@@ -109,7 +110,7 @@ private:
 class TimeUpdateEvent : public QEvent
 {
 public:
-    TimeUpdateEvent(const std::vector<double*> timeDomainPoints,
+    TimeUpdateEvent(const std::vector<volk::vector<double>> timeDomainPoints,
                     const uint64_t numTimeDomainDataPoints,
                     const std::vector<std::vector<gr::tag_t>> tags);
 
@@ -139,7 +140,8 @@ private:
 class FreqUpdateEvent : public QEvent
 {
 public:
-    FreqUpdateEvent(const std::vector<double*> dataPoints, const uint64_t numDataPoints);
+    FreqUpdateEvent(const std::vector<volk::vector<double>> dataPoints,
+                    const uint64_t numDataPoints);
 
     ~FreqUpdateEvent() override;
 
@@ -178,8 +180,8 @@ private:
 class QTGUI_API ConstUpdateEvent : public QEvent
 {
 public:
-    ConstUpdateEvent(const std::vector<double*> realDataPoints,
-                     const std::vector<double*> imagDataPoints,
+    ConstUpdateEvent(const std::vector<volk::vector<double>> realDataPoints,
+                     const std::vector<volk::vector<double>> imagDataPoints,
                      const uint64_t numDataPoints);
 
     ~ConstUpdateEvent() override;
@@ -207,7 +209,7 @@ private:
 class WaterfallUpdateEvent : public QEvent
 {
 public:
-    WaterfallUpdateEvent(const std::vector<double*> dataPoints,
+    WaterfallUpdateEvent(const std::vector<volk::vector<double>> dataPoints,
                          const uint64_t numDataPoints,
                          const gr::high_res_timer_type dataTimestamp);
 
@@ -238,7 +240,7 @@ private:
 class TimeRasterUpdateEvent : public QEvent
 {
 public:
-    TimeRasterUpdateEvent(const std::vector<double*> dataPoints,
+    TimeRasterUpdateEvent(const std::vector<volk::vector<double>> dataPoints,
                           const uint64_t numDataPoints);
     ~TimeRasterUpdateEvent() override;
 
@@ -280,7 +282,8 @@ private:
 class HistogramUpdateEvent : public QEvent
 {
 public:
-    HistogramUpdateEvent(const std::vector<double*> points, const uint64_t npoints);
+    HistogramUpdateEvent(const std::vector<volk::vector<double>> points,
+                         const uint64_t npoints);
 
     ~HistogramUpdateEvent() override;
 

gr-qtgui/lib/ber_sink_b_impl.cc
@@ -61,10 +61,8 @@ ber_sink_b_impl::ber_sink_b_impl(std::vector<float> esnos,
     d_total_errors.reserve(curves * esnos.size());
 
     for (int j = 0; j < curves; j++) {
-        d_esno_buffers.push_back(
-            (double*)volk_malloc(esnos.size() * sizeof(double), volk_get_alignment()));
-        d_ber_buffers.push_back(
-            (double*)volk_malloc(esnos.size() * sizeof(double), volk_get_alignment()));
+        d_esno_buffers.emplace_back(esnos.size());
+        d_ber_buffers.emplace_back(esnos.size());
 
         for (int i = 0; i < d_nconnections; i++) {
             d_esno_buffers[j][i] = esnos[i];
@@ -75,10 +73,8 @@ ber_sink_b_impl::ber_sink_b_impl(std::vector<float> esnos,
     }
 
     // Now add the known curves
-    d_esno_buffers.push_back(
-        (double*)volk_malloc(esnos.size() * sizeof(double), volk_get_alignment()));
-    d_ber_buffers.push_back(
-        (double*)volk_malloc(esnos.size() * sizeof(double), volk_get_alignment()));
+    d_esno_buffers.emplace_back(esnos.size());
+    d_ber_buffers.emplace_back(esnos.size());
     for (size_t i = 0; i < esnos.size(); i++) {
         double e = pow(10.0, esnos[i] / 10.0);
         d_esno_buffers[curves][i] = esnos[i];
@@ -113,11 +109,6 @@ ber_sink_b_impl::~ber_sink_b_impl()
 {
     if (!d_main_gui->isClosed()) {
         d_main_gui->close();
-    }
-
-    for (unsigned int i = 0; i < d_esno_buffers.size(); i++) {
-        volk_free(d_esno_buffers[i]);
-        volk_free(d_ber_buffers[i]);
     }
 }
 

gr-qtgui/lib/ber_sink_b_impl.h
@@ -16,6 +16,8 @@
 #include <gnuradio/high_res_timer.h>
 #include <gnuradio/qtgui/constellationdisplayform.h>
 
+#include <volk/volk_alloc.hh>
+
 namespace gr {
 namespace qtgui {
 
@@ -24,10 +26,10 @@ class QTGUI_API ber_sink_b_impl : public ber_sink_b
 private:
     void initialize();
 
-    std::vector<double*> d_esno_buffers;
-    std::vector<double*> d_ber_buffers;
+    std::vector<volk::vector<double>> d_esno_buffers;
+    std::vector<volk::vector<double>> d_ber_buffers;
 
-    ConstellationDisplayForm* d_main_gui;
+    ConstellationDisplayForm* d_main_gui = nullptr;
     gr::high_res_timer_type d_update_time;
     std::vector<int> d_total_errors;
     int d_ber_min_errors;
@@ -48,6 +50,12 @@ public:
                     std::vector<std::string> curvenames = std::vector<std::string>(),
                     QWidget* parent = NULL);
     ~ber_sink_b_impl() override;
+
+    // Disallow copy/move because of the raw pointers.
+    ber_sink_b_impl(const ber_sink_b_impl&) = delete;
+    ber_sink_b_impl& operator=(const ber_sink_b_impl&) = delete;
+    ber_sink_b_impl(ber_sink_b_impl&&) = delete;
+    ber_sink_b_impl& operator=(ber_sink_b_impl&&) = delete;
 
     bool check_topology(int ninputs, int noutputs) override;
 

gr-qtgui/lib/const_sink_c_impl.cc
@@ -18,6 +18,7 @@
 #include <gnuradio/prefs.h>
 #include <qwt_symbol.h>
 #include <volk/volk.h>
+#include <volk/volk_alloc.hh>
 #include <cstring>
 
 namespace gr {
@@ -42,38 +43,14 @@ const_sink_c_impl::const_sink_c_impl(int size,
       d_nconnections(nconnections),
       d_parent(parent)
 {
-    // Required now for Qt; argc must be greater than 0 and argv
-    // must have at least one valid character. Must be valid through
-    // life of the qApplication:
-    // http://harmattan-dev.nokia.com/docs/library/html/qt4/qapplication.html
-    d_argc = 1;
-    d_argv = new char;
-    d_argv[0] = '\0';
-
-    d_main_gui = NULL;
-
-    d_index = 0;
-
     // setup PDU handling input port
     message_port_register_in(pmt::mp("in"));
     set_msg_handler(pmt::mp("in"), [this](pmt::pmt_t msg) { this->handle_pdus(msg); });
 
-    for (int i = 0; i < d_nconnections; i++) {
-        d_residbufs_real.push_back(
-            (double*)volk_malloc(d_buffer_size * sizeof(double), volk_get_alignment()));
-        d_residbufs_imag.push_back(
-            (double*)volk_malloc(d_buffer_size * sizeof(double), volk_get_alignment()));
-        memset(d_residbufs_real[i], 0, d_buffer_size * sizeof(double));
-        memset(d_residbufs_imag[i], 0, d_buffer_size * sizeof(double));
+    for (int i = 0; i < d_nconnections + 1; i++) {
+        d_residbufs_real.emplace_back(d_buffer_size);
+        d_residbufs_imag.emplace_back(d_buffer_size);
     }
-
-    // Used for PDU message input
-    d_residbufs_real.push_back(
-        (double*)volk_malloc(d_buffer_size * sizeof(double), volk_get_alignment()));
-    d_residbufs_imag.push_back(
-        (double*)volk_malloc(d_buffer_size * sizeof(double), volk_get_alignment()));
-    memset(d_residbufs_real[d_nconnections], 0, d_buffer_size * sizeof(double));
-    memset(d_residbufs_imag[d_nconnections], 0, d_buffer_size * sizeof(double));
 
     // Set alignment properties for VOLK
     const int alignment_multiple = volk_get_alignment() / sizeof(gr_complex);
@@ -90,14 +67,6 @@ const_sink_c_impl::~const_sink_c_impl()
 {
     if (!d_main_gui->isClosed())
         d_main_gui->close();
-
-    // d_main_gui is a qwidget destroyed with its parent
-    for (int i = 0; i < d_nconnections + 1; i++) {
-        volk_free(d_residbufs_real[i]);
-        volk_free(d_residbufs_imag[i]);
-    }
-
-    delete d_argv;
 }
 
 bool const_sink_c_impl::check_topology(int ninputs, int noutputs)
@@ -276,15 +245,10 @@ void const_sink_c_impl::set_nsamps(const int newsize)
         // Resize residbuf and replace data
         // +1 to handle PDU message input buffers
         for (int i = 0; i < d_nconnections + 1; i++) {
-            volk_free(d_residbufs_real[i]);
-            volk_free(d_residbufs_imag[i]);
-            d_residbufs_real[i] = (double*)volk_malloc(d_buffer_size * sizeof(double),
-                                                       volk_get_alignment());
-            d_residbufs_imag[i] = (double*)volk_malloc(d_buffer_size * sizeof(double),
-                                                       volk_get_alignment());
-
-            memset(d_residbufs_real[i], 0, d_buffer_size * sizeof(double));
-            memset(d_residbufs_imag[i], 0, d_buffer_size * sizeof(double));
+            d_residbufs_real[i].clear();
+            d_residbufs_imag[i].clear();
+            d_residbufs_real[i].resize(d_buffer_size);
+            d_residbufs_imag[i].resize(d_buffer_size);
         }
 
         d_main_gui->setNPoints(d_size);
@@ -437,10 +401,10 @@ int const_sink_c_impl::work(int noutput_items,
     if ((d_triggered) && (d_index == d_end)) {
         // Copy data to be plotted to start of buffers.
         for (n = 0; n < d_nconnections; n++) {
-            memmove(d_residbufs_real[n],
+            memmove(d_residbufs_real[n].data(),
                     &d_residbufs_real[n][d_start],
                     d_size * sizeof(double));
-            memmove(d_residbufs_imag[n],
+            memmove(d_residbufs_imag[n].data(),
                     &d_residbufs_imag[n][d_start],
                     d_size * sizeof(double));
         }
@@ -500,8 +464,10 @@ void const_sink_c_impl::handle_pdus(pmt::pmt_t msg)
         d_last_time = gr::high_res_timer_now();
 
         // Copy data into the buffers.
-        volk_32fc_deinterleave_64f_x2(
-            d_residbufs_real[d_nconnections], d_residbufs_imag[d_nconnections], in, len);
+        volk_32fc_deinterleave_64f_x2(d_residbufs_real[d_nconnections].data(),
+                                      d_residbufs_imag[d_nconnections].data(),
+                                      in,
+                                      len);
 
         d_qApplication->postEvent(
             d_main_gui, new ConstUpdateEvent(d_residbufs_real, d_residbufs_imag, len));

gr-qtgui/lib/const_sink_c_impl.h
@@ -28,14 +28,21 @@ private:
     std::string d_name;
     int d_nconnections;
 
-    int d_index, d_start, d_end;
-    std::vector<double*> d_residbufs_real;
-    std::vector<double*> d_residbufs_imag;
+    int d_index = 0;
+    int d_start;
+    int d_end;
+    std::vector<volk::vector<double>> d_residbufs_real;
+    std::vector<volk::vector<double>> d_residbufs_imag;
 
-    int d_argc;
-    char* d_argv;
+    // Required now for Qt; argc must be greater than 0 and argv
+    // must have at least one valid character. Must be valid through
+    // life of the qApplication:
+    // http://harmattan-dev.nokia.com/docs/library/html/qt4/qapplication.html
+    char d_zero = 0;
+    int d_argc = 1;
+    char* d_argv = &d_zero;
     QWidget* d_parent;
-    ConstellationDisplayForm* d_main_gui;
+    ConstellationDisplayForm* d_main_gui = nullptr;
 
     gr::high_res_timer_type d_update_time;
     gr::high_res_timer_type d_last_time;
@@ -65,6 +72,12 @@ public:
                       int nconnections,
                       QWidget* parent = NULL);
     ~const_sink_c_impl() override;
+
+    // Disallow copy/move because of the pointers.
+    const_sink_c_impl(const const_sink_c_impl&) = delete;
+    const_sink_c_impl& operator=(const const_sink_c_impl&) = delete;
+    const_sink_c_impl(const_sink_c_impl&&) = delete;
+    const_sink_c_impl& operator=(const_sink_c_impl&&) = delete;
 
     bool check_topology(int ninputs, int noutputs) override;
 

gr-qtgui/lib/edit_box_msg_impl.cc
@@ -45,14 +45,6 @@ edit_box_msg_impl::edit_box_msg_impl(data_type_t type,
       QObject(parent),
       d_port(pmt::mp("msg"))
 {
-    // Required now for Qt; argc must be greater than 0 and argv
-    // must have at least one valid character. Must be valid through
-    // life of the qApplication:
-    // http://harmattan-dev.nokia.com/docs/library/html/qt4/qapplication.html
-    d_argc = 1;
-    d_argv = new char;
-    d_argv[0] = '\0';
-
     if (qApp != NULL) {
         d_qApplication = qApp;
     } else {

commit 4cbdbfa571a675324f86f769b11e9f5d54bd4c2b

qtgui: Fix segfaulting overflow
Problem introduced in
1187e66f23b4408ef2e1accdfb3fcb3e1bd6eea0. `d_cbuffers` and `d_buffers`
are not the same size.

It appears to be the only instance of this mistake in that commit.

Fixes: #4396
Signed-off-by: Thomas Habets <habets@google.com>

gr-qtgui/lib/time_sink_c_impl.cc
@@ -56,10 +56,6 @@ time_sink_c_impl::time_sink_c_impl(int size,
     if (nconnections > 12)
         throw std::runtime_error("time_sink_c only supports up to 12 inputs");
 
-    // setup PDU handling input port
-    message_port_register_in(pmt::mp("in"));
-    set_msg_handler(pmt::mp("in"), [this](pmt::pmt_t msg) { this->handle_pdus(msg); });
-
     // +2 for the PDU message buffers
     for (unsigned int n = 0; n < d_nconnections + 2; n++) {
         d_buffers.emplace_back(d_buffer_size);
@@ -69,6 +65,10 @@ time_sink_c_impl::time_sink_c_impl(int size,
     for (unsigned int n = 0; n < d_nconnections / 2; n++) {
         d_cbuffers.emplace_back(d_buffer_size);
     }
+
+    // setup PDU handling input port
+    message_port_register_in(pmt::mp("in"));
+    set_msg_handler(pmt::mp("in"), [this](pmt::pmt_t msg) { this->handle_pdus(msg); });
 
     // Set alignment properties for VOLK
     const int alignment_multiple = volk_get_alignment() / sizeof(gr_complex);
@@ -277,11 +277,14 @@ void time_sink_c_impl::set_nsamps(const int newsize)
         d_buffer_size = 2 * d_size;
 
         // Resize buffers and replace data
-        for (unsigned int n = 0; n < d_nconnections + 2; n++) {
-            d_buffers[n].clear();
-            d_buffers[n].resize(d_buffer_size);
-            d_cbuffers[n].clear();
-            d_cbuffers[n].resize(d_buffer_size);
+        for (auto& buf : d_buffers) {
+            buf.clear();
+            buf.resize(d_buffer_size);
+        }
+
+        for (auto& cbuf : d_cbuffers) {
+            cbuf.clear();
+            cbuf.resize(d_buffer_size);
         }
 
         // If delay was set beyond the new boundary, pull it back.