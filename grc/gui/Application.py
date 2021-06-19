krisplusmagger/radio

commit 0cead20d3a7df6e17cb522cf2b11825f07348d73

grc: Fix multiple 'Apply' in modify block parameters dialog box
When the modify parameters dialog block is constructed, the instance of
the block to be modified, is passed in. When the apply button is clicked
there is an extraenous flowgraph.import_data() call that re-creates all
blocks in the flowgraph. The consequence is that the block instance
passed to the dialog box is no longer the one in the flowgraph, and so
subsequent Applies have no effect. This change simply removes this call.

grc/gui/Application.py
@@ -544,11 +544,8 @@ class Application(Gtk.Application):
                     response = self.dialog.run()
                     if response in (Gtk.ResponseType.APPLY, Gtk.ResponseType.ACCEPT):
                         page.state_cache.save_new_state(flow_graph.export_data())
-                        ### Following  lines force an complete update of io ports
-                        n = page.state_cache.get_current_state()
-                        flow_graph.import_data(n)
+                        ### Following  line forces a complete update of io ports
                         flow_graph_update()
-
                         page.saved = False
                     else:  # restore the current state
                         n = page.state_cache.get_current_state()

commit 1fd6fcf414fd1fb71e5904f3d49cfad19516f49f

grc: Update flowgraph vars controlled by vars
Fixes #4781. When a variable is controlled by another variable,
updating the latter does not update the former in the flowgraph when
`Ok` is pressed. Caused by commit 0cead20. This commit patches the bug.

Signed-off-by: Solomon Tan <solomonbstoner@yahoo.com.au>

grc/gui/Application.py
@@ -557,7 +557,7 @@ class Application(Gtk.Application):
                         ### Following line forces a complete update of io ports
                         flow_graph_update()
                         page.saved = False
-                    else:  # restore the current state
+                    if response in (Gtk.ResponseType.REJECT, Gtk.ResponseType.ACCEPT):
                         n = page.state_cache.get_current_state()
                         flow_graph.import_data(n)
                         flow_graph_update()