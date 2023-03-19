krisplusmagger/radio

commit eaa110d9013f88f03029a99231a2e44d8aabc838

fec: fix some bugs and refactor async_decoder
This fixes a few bugs and shortcomings in async_decoder and refactors
it to reduce code duplication by merging the decode_unpacked() and
decoded_packed() functions into a single decode() function.

The problems solved are the following:

- decode_packed() wasn't considering the 'diff' (Viterbi tail) properly,
  unlike decode_unpacked(). This caused undefined behaviour when working
  in packed output mode with the Viterbi decoder in terminated mode.
  Occasionally this would cause bit errors in the last bits of a packet.
  See #6445 for a very similar problem affecting tagged_decoder.

- decode_packed() didn't support multiple blocks for fixed-size decoders
  (unlike decode_unpacked()).

- In both decode_unpacked() and decode_packed(), the input conversion
  to uint8_t could overflow (see #6433). The behaviour has been changed
  to clipping, which is less harmful.

Taking into account that some of these problems affected decode_packed() and not
decode_unpacked() because the two functions were quite similar but had some
differences, it seems a good idea to reduce the code duplication by merging the
two functions into one.

Additionally, there are these improvements:

- The opportunity to use volk_32f_s32f_x2_convert_8u (when it is released)
  for the input conversion to uint8_t has been identified in a comment.

- When there is a decoder shift and no input conversion, the shift is
  applied with volk_32f_s32f_add_32f rather than with an
  element-by-element loop.

Signed-off-by: Daniel Estévez <daniel@destevez.net>

gr-fec/lib/async_decoder_impl.cc
@@ -51,12 +51,7 @@ async_decoder_impl::async_decoder_impl(generic_decoder::sptr my_decoder,
     message_port_register_in(d_in_port);
     message_port_register_out(d_out_port);
 
-    if (d_packed) {
-        set_msg_handler(d_in_port, [this](pmt::pmt_t msg) { this->decode_packed(msg); });
-    } else {
-        set_msg_handler(d_in_port,
-                        [this](pmt::pmt_t msg) { this->decode_unpacked(msg); });
-    }
+    set_msg_handler(d_in_port, [this](const pmt::pmt_t& msg) { this->decode(msg); });
 
     // The maximum frame size is set by the initial frame size of the decoder.
     d_max_bits_in = d_mtu * 8 * 1.0 / d_decoder->rate();
@@ -74,7 +69,26 @@ async_decoder_impl::async_decoder_impl(generic_decoder::sptr my_decoder,
 
 async_decoder_impl::~async_decoder_impl() {}
 
-void async_decoder_impl::decode_unpacked(pmt::pmt_t msg)
+// TODO: replace this function by volk_32f_s32f_x2_convert_8u when it gets into
+// a stable volk release
+inline void async_decoder_impl::convert_32f_to_8u(uint8_t* output_vector,
+                                                  const float* input_vector,
+                                                  const float scale,
+                                                  const float bias,
+                                                  unsigned int num_points)
+{
+    volk_32f_s32f_multiply_32f(d_tmp_f32.data(), input_vector, scale, num_points);
+    if (bias != 0.0) {
+        for (size_t n = 0; n < num_points; n++) {
+            d_tmp_f32[n] += bias;
+        }
+    }
+    for (size_t n = 0; n < num_points; n++) {
+        output_vector[n] = std::clamp<float>(d_tmp_f32[n], 0.0f, UINT8_MAX);
+    }
+}
+
+void async_decoder_impl::decode(const pmt::pmt_t& msg)
 {
     // extract input pdu
     pmt::pmt_t meta(pmt::car(msg));
@@ -110,89 +124,47 @@ void async_decoder_impl::decode_unpacked(pmt::pmt_t msg)
         }
     }
 
-    size_t o0(0);
-    const float* f32in = pmt::f32vector_elements(bits, o0);
-    pmt::pmt_t outvec(pmt::make_u8vector(nbits_out, 0x00));
-    uint8_t* u8out = pmt::u8vector_writable_elements(outvec, o0);
+    size_t vector_len = 0; // used only to get the length of PMT vectors
+    const float* f32in = pmt::f32vector_elements(bits, vector_len);
+    const size_t nitems_out = d_packed ? nbits_out / 8 : nbits_out;
+    auto outvec = pmt::make_u8vector(nitems_out, 0x00);
+    uint8_t* u8out = pmt::u8vector_writable_elements(outvec, vector_len);
+    uint8_t* decoder_out = d_packed ? d_bits_out.data() : u8out;
+    const float shift = d_decoder->get_shift();
 
-    if (strncmp(d_decoder->get_input_conversion(), "uchar", 5) == 0) {
-        volk_32f_s32f_multiply_32f(d_tmp_f32.data(), f32in, 48.0f, nbits_in);
-    } else {
-        memcpy(d_tmp_f32.data(), f32in, nbits_in * sizeof(float));
-    }
+    if (std::string_view(d_decoder->get_input_conversion()) == "uchar") {
+        convert_32f_to_8u(d_tmp_u8.data(), f32in, 48.0f, shift, nitems_out);
 
-    if (d_decoder->get_shift() != 0) {
-        for (size_t n = 0; n < nbits_in; n++)
-            d_tmp_f32[n] += d_decoder->get_shift();
-    }
-
-    if (strncmp(d_decoder->get_input_conversion(), "uchar", 5) == 0) {
-        // volk_32f_s32f_convert_8i(d_tmp_u8, d_tmp_f32, 1, nbits_in);
-        for (size_t n = 0; n < nbits_in; n++)
-            d_tmp_u8[n] = static_cast<uint8_t>(d_tmp_f32[n]);
-
-        d_decoder->generic_work((void*)d_tmp_u8.data(), (void*)u8out);
-    } else {
         for (size_t i = 0; i < nblocks; i++) {
-            d_decoder->generic_work((void*)&d_tmp_f32[i * d_decoder->get_input_size()],
-                                    (void*)&u8out[i * d_decoder->get_output_size()]);
+            d_decoder->generic_work(
+                (void*)&d_tmp_u8[i * d_decoder->get_input_size()],
+                (void*)&decoder_out[i * d_decoder->get_output_size()]);
+        }
+    } else {
+        if (shift != 0.0) {
+            volk_32f_s32f_add_32f(d_tmp_f32.data(), f32in, shift, nbits_in);
+        } else {
+            memcpy(d_tmp_f32.data(), f32in, nbits_in * sizeof(float));
+        }
+
+        for (size_t i = 0; i < nblocks; i++) {
+            d_decoder->generic_work(
+                (void*)&d_tmp_f32[i * d_decoder->get_input_size()],
+                (void*)&decoder_out[i * d_decoder->get_output_size()]);
         }
     }
 
-    static const pmt::pmt_t iterations_key = pmt::mp("iterations");
-    meta = pmt::dict_add(meta, iterations_key, pmt::mp(d_decoder->get_iterations()));
-    message_port_pub(d_out_port, pmt::cons(meta, outvec));
-}
+    // Note: when nblocks > 1, this only gets the iterations for the last
+    // decoder block.
+    meta = pmt::dict_add(meta, ITERATIONS_KEY, pmt::mp(d_decoder->get_iterations()));
 
-void async_decoder_impl::decode_packed(pmt::pmt_t msg)
-{
-    // extract input pdu
-    pmt::pmt_t meta(pmt::car(msg));
-    pmt::pmt_t bits(pmt::cdr(msg));
-
-    size_t o0 = 0;
-    size_t nbits_in = pmt::length(bits);
-    int nbits_out = nbits_in * d_decoder->rate();
-    int nbytes_out = nbits_out / 8;
-
-    // Check here if the frame size is larger than what we've
-    // allocated for in the constructor.
-    if (nbits_in > d_max_bits_in) {
-        throw std::runtime_error(
-            "async_decoder: Received frame larger than max frame size.");
+    if (d_packed) {
+        if (d_rev_pack) {
+            d_pack.pack_rev(u8out, decoder_out, nitems_out);
+        } else {
+            d_pack.pack(u8out, decoder_out, nitems_out);
+        }
     }
-
-    d_decoder->set_frame_size(nbits_out);
-
-    pmt::pmt_t outvec(pmt::make_u8vector(nbytes_out, 0x00));
-    uint8_t* bytes_out = pmt::u8vector_writable_elements(outvec, o0);
-    const float* f32in = pmt::f32vector_elements(bits, o0);
-
-    if (strncmp(d_decoder->get_input_conversion(), "uchar", 5) == 0) {
-        volk_32f_s32f_multiply_32f(d_tmp_f32.data(), f32in, 48.0f, nbits_in);
-    } else {
-        memcpy(d_tmp_f32.data(), f32in, nbits_in * sizeof(float));
-    }
-
-    if (d_decoder->get_shift() != 0) {
-        for (size_t n = 0; n < nbits_in; n++)
-            d_tmp_f32[n] += d_decoder->get_shift();
-    }
-
-    if (strncmp(d_decoder->get_input_conversion(), "uchar", 5) == 0) {
-        // volk_32f_s32f_convert_8i(d_tmp_u8, d_tmp_f32, 1.0, nbits_in);
-        for (size_t n = 0; n < nbits_in; n++)
-            d_tmp_u8[n] = static_cast<uint8_t>(d_tmp_f32[n]);
-
-        d_decoder->generic_work((void*)d_tmp_u8.data(), (void*)d_bits_out.data());
-    } else {
-        d_decoder->generic_work((void*)d_tmp_f32.data(), (void*)d_bits_out.data());
-    }
-
-    if (d_rev_pack)
-        d_pack.pack_rev(bytes_out, d_bits_out.data(), nbytes_out);
-    else
-        d_pack.pack(bytes_out, d_bits_out.data(), nbytes_out);
 
     message_port_pub(d_out_port, pmt::cons(meta, outvec));
 }

gr-fec/lib/async_decoder_impl.h
@@ -18,6 +18,8 @@
 namespace gr {
 namespace fec {
 
+static const pmt::pmt_t ITERATIONS_KEY = pmt::mp("iterations");
+
 class FEC_API async_decoder_impl : public async_decoder
 {
 private:
@@ -34,11 +36,16 @@ private:
 
     size_t d_max_bits_in;
     volk::vector<float> d_tmp_f32;
-    volk::vector<int8_t> d_tmp_u8;
+    volk::vector<uint8_t> d_tmp_u8;
     volk::vector<uint8_t> d_bits_out;
 
-    void decode_packed(pmt::pmt_t msg);
-    void decode_unpacked(pmt::pmt_t msg);
+    void decode(const pmt::pmt_t& msg);
+
+    inline void convert_32f_to_8u(uint8_t* output_vector,
+                                  const float* input_vector,
+                                  const float scale,
+                                  const float bias,
+                                  unsigned int num_points);
 
 public:
     async_decoder_impl(generic_decoder::sptr my_decoder,

commit cad78e81aeb9112104b6bfa638446599a109bcf5

fec: fix regression in async_decoder and add tests
The async_decoder was broken in eaa110d9013f88f03029a99231a2e44d8aabc838.
The reason was a mistake when rewriting the PR to address comments.
In the case where the decoder input needs to be converted to uchar,
'nitems_out' rather than 'nbits_in' was wrongly used as the length
of the data to be converted. Effectively, we are passing only the
first half of the input to a rate=1/2 decoder such as the CC decoder.

Since there are no tests of the async_decoder that would have caught
the bug, this adds some tests. A new _qa_helper_async top_block similar
to _qa_helper is added. This is used in an additional test case in the
qa_fecapi_* tests to run the async_encoder and async_decoder with all the
different FECAPI encoders/decoders.

Signed-off-by: Daniel Estévez <daniel@destevez.net>

gr-fec/lib/async_decoder_impl.cc
@@ -133,7 +133,7 @@ void async_decoder_impl::decode(const pmt::pmt_t& msg)
     const float shift = d_decoder->get_shift();
 
     if (std::string_view(d_decoder->get_input_conversion()) == "uchar") {
-        convert_32f_to_8u(d_tmp_u8.data(), f32in, 48.0f, shift, nitems_out);
+        convert_32f_to_8u(d_tmp_u8.data(), f32in, 48.0f, shift, nbits_in);
 
         for (size_t i = 0; i < nblocks; i++) {
             d_decoder->generic_work(

gr-fec/python/fec/_qa_helper_async.py
@@ -1,0 +1,108 @@
+#!/usr/bin/env python
+#
+# Copyright 2014 Free Software Foundation, Inc.
+# Copyright 2023 Daniel Estevez <daniel@destevez.net>
+#
+# This file is part of GNU Radio
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+#
+
+# This is similar to _qa_helper.py but uses async_encoder and async_decoder
+# instead of extended_encoder and extended_decoder.
+
+
+import numpy as np
+
+from gnuradio import gr, blocks, fec, pdu
+# Must use absolute import here because this file is not installed as part
+# of the module
+from gnuradio.fec import async_encoder, async_decoder
+import pmt
+
+from _qa_helper import map_bb
+
+
+class _qa_helper_async(gr.top_block):
+
+    def __init__(self, frame_size, enc, dec, packed, rev_pack):
+        gr.top_block.__init__(self, "_qa_helper_async")
+
+        self.enc = enc
+        self.dec = dec
+        self.frame_size = frame_size
+        # These options are for the decoder only. The encoder is always run in
+        # unpacked mode.
+        self.packed = packed
+        self.rev_pack = rev_pack
+
+        self.async_encoder = async_encoder(enc)
+        self.async_decoder = async_decoder(
+            dec, packed=packed, rev_pack=rev_pack)
+
+        num_frames = 64
+        frame_size_bits = 8 * frame_size
+        data_in = np.random.randint(
+            2, size=frame_size_bits * num_frames, dtype='uint8')
+        tags = [
+            gr.tag_utils.python_to_tag((
+                frame_size_bits * j, pmt.intern('packet_len'),
+                pmt.from_long(frame_size_bits), pmt.intern('src')))
+            for j in range(num_frames)]
+        self.src = blocks.vector_source_b(data_in, False, tags=tags)
+        self.tagged_to_pdu_enc = pdu.tagged_stream_to_pdu(
+            gr.types.byte_t, 'packet_len')
+        self.pdu_to_tagged_enc = pdu.pdu_to_tagged_stream(
+            gr.types.byte_t, 'packet_len')
+        self.map = map_bb([-1, 1])
+        self.to_float = blocks.char_to_float(1)
+        self.tagged_to_pdu_dec = pdu.tagged_stream_to_pdu(
+            gr.types.float_t, 'packet_len')
+        self.pdu_to_tagged_dec = pdu.pdu_to_tagged_stream(
+            gr.types.byte_t, 'packet_len')
+        self.snk_input = blocks.vector_sink_b()
+        self.snk_output = blocks.vector_sink_b()
+        if packed:
+            self.unpack_decoder = blocks.packed_to_unpacked_bb(
+                1, gr.GR_LSB_FIRST if rev_pack else gr.GR_MSB_FIRST)
+
+        self.connect(self.src, self.tagged_to_pdu_enc)
+        self.msg_connect((self.tagged_to_pdu_enc, 'pdus'),
+                         (self.async_encoder, 'in'))
+        self.msg_connect((self.async_encoder, 'out'),
+                         (self.pdu_to_tagged_enc, 'pdus'))
+        self.connect(
+            self.pdu_to_tagged_enc, self.map, self.to_float,
+            self.tagged_to_pdu_dec)
+        self.msg_connect((self.tagged_to_pdu_dec, 'pdus'),
+                         (self.async_decoder, 'in'))
+        self.msg_connect((self.async_decoder, 'out'),
+                         (self.pdu_to_tagged_dec, 'pdus'))
+        self.connect(self.src, self.snk_input)
+        if packed:
+            self.connect(
+                self.pdu_to_tagged_dec, self.unpack_decoder, self.snk_output)
+        else:
+            self.connect(self.pdu_to_tagged_dec, self.snk_output)
+
+
+if __name__ == '__main__':
+    frame_size = 30
+    enc = fec.dummy_encoder_make(frame_size * 8)
+    dec = fec.dummy_decoder.make(frame_size * 8)
+
+    for packed in [True, False]:
+        for rev_pack in [True, False]:
+            tb = _qa_helper_async(frame_size, enc, dec, packed, rev_pack)
+            tb.run()
+
+            errs = 0
+            for i, o in zip(tb.snk_input.data(), tb.snk_output.data()):
+                if i - o != 0:
+                    errs += 1
+
+            if errs == 0:
+                print("Decoded properly")
+            else:
+                print("Problem Decoding")

gr-fec/python/fec/qa_fecapi_cc.py
@@ -13,6 +13,7 @@ from gnuradio import gr, gr_unittest
 from gnuradio import fec
 
 from _qa_helper import _qa_helper
+from _qa_helper_async import _qa_helper_async
 
 
 class test_fecapi_cc(gr_unittest.TestCase):
@@ -190,6 +191,28 @@ class test_fecapi_cc(gr_unittest.TestCase):
         data_in = self.test.snk_input.data()[0:len(data_out)]
 
         self.assertEqual(data_in, data_out)
+
+    def test_async_00(self):
+        frame_size = 30
+        k = 7
+        rate = 2
+        polys = [109, 79]
+        for mode in [fec.CC_TRUNCATED, fec.CC_TERMINATED, fec.CC_TAILBITING]:
+            for packed in [True, False]:
+                for rev_pack in [True, False]:
+                    with self.subTest(mode=mode, packed=packed,
+                                      rev_pack=rev_pack):
+                        enc = fec.cc_encoder_make(
+                            frame_size * 8, k, rate, polys, mode=mode)
+                        dec = fec.cc_decoder.make(
+                            frame_size * 8, k, rate, polys, mode=mode)
+                        self.test = _qa_helper_async(
+                            frame_size, enc, dec, packed, rev_pack)
+                        self.test.run()
+
+                        data_in = self.test.snk_input.data()
+                        data_out = self.test.snk_output.data()
+                        self.assertEqual(data_in, data_out)
 
 
 if __name__ == '__main__':

gr-fec/python/fec/qa_fecapi_dummy.py
@@ -17,6 +17,7 @@ from gnuradio.fec import extended_encoder
 from gnuradio.fec import extended_decoder
 
 from _qa_helper import _qa_helper
+from _qa_helper_async import _qa_helper_async
 
 
 class test_fecapi_dummy(gr_unittest.TestCase):
@@ -260,6 +261,22 @@ class test_fecapi_dummy(gr_unittest.TestCase):
         self.assertSequenceEqualGR(data, r1)
         self.assertSequenceEqualGR(packed_data, r2)
         self.assertSequenceEqualGR(data, r3)
+
+    def test_async_00(self):
+        frame_size = 30
+        enc = fec.dummy_encoder_make(frame_size * 8)
+        dec = fec.dummy_decoder.make(frame_size * 8)
+        for packed in [True, False]:
+            for rev_pack in [True, False]:
+                with self.subTest(packed=packed, rev_pack=rev_pack):
+                    self.test = _qa_helper_async(
+                        frame_size, enc, dec, packed, rev_pack)
+                    self.test.run()
+
+                    data_in = self.test.snk_input.data()
+                    data_out = self.test.snk_output.data()
+
+                    self.assertSequenceEqualGR(data_in, data_out)
 
 
 if __name__ == '__main__':

gr-fec/python/fec/qa_fecapi_ldpc.py
@@ -17,6 +17,7 @@ from gnuradio.fec import extended_encoder
 from gnuradio.fec import extended_decoder
 
 from _qa_helper import _qa_helper
+from _qa_helper_async import _qa_helper_async
 
 
 # Get location of the alist files. If run in 'ctest' or 'make test',
@@ -307,6 +308,25 @@ class test_fecapi_ldpc(gr_unittest.TestCase):
                 dec,
                 threading=threading,
                 puncpat="11"))
+
+    def test_async_00(self):
+        filename = LDPC_ALIST_DIR + "n_0100_k_0027_gap_04.alist"
+        gap = 4
+        LDPC_matrix_object = fec.ldpc_H_matrix(filename, gap)
+        k = LDPC_matrix_object.k()
+        enc = fec.ldpc_par_mtrx_encoder.make_H(LDPC_matrix_object)
+        dec = fec.ldpc_bit_flip_decoder.make(
+            LDPC_matrix_object.get_base_sptr())
+        for packed in [True, False]:
+            for rev_pack in [True, False]:
+                with self.subTest(packed=packed, rev_pack=rev_pack):
+                    self.test = _qa_helper_async(k, enc, dec, packed, rev_pack)
+                    self.test.run()
+
+                    data_in = self.test.snk_input.data()
+                    data_out = self.test.snk_output.data()
+
+                    self.assertEqual(data_in, data_out)
 
 
 if __name__ == '__main__':

gr-fec/python/fec/qa_fecapi_repetition.py
@@ -13,6 +13,7 @@ from gnuradio import gr, gr_unittest
 from gnuradio import fec
 
 from _qa_helper import _qa_helper
+from _qa_helper_async import _qa_helper_async
 
 
 class test_fecapi_repetition(gr_unittest.TestCase):
@@ -154,6 +155,23 @@ class test_fecapi_repetition(gr_unittest.TestCase):
         data_out = self.test.snk_output.data()
 
         self.assertSequenceEqualGR(data_in, data_out)
+
+    def test_async_00(self):
+        frame_size = 30
+        rep = 3
+        enc = fec.repetition_encoder_make(frame_size * 8, rep)
+        dec = fec.repetition_decoder.make(frame_size * 8, rep)
+        for packed in [True, False]:
+            for rev_pack in [True, False]:
+                with self.subTest(packed=packed, rev_pack=rev_pack):
+                    self.test = _qa_helper_async(
+                        frame_size, enc, dec, packed, rev_pack)
+                    self.test.run()
+
+                    data_in = self.test.snk_input.data()
+                    data_out = self.test.snk_output.data()
+
+                    self.assertSequenceEqualGR(data_in, data_out)
 
 
 if __name__ == '__main__':